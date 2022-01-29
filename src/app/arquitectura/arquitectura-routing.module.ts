855040d34209453d4b1009db6e2ad9b528f7e877
AndresGonzalez <andresfelgonta@gamil.com>
2022-01-29 15:19:22 -0500

    Index and contact

--- src/app/arquitectura/arquitectura-routing.module.ts
+++ src/app/arquitectura/arquitectura-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AboutUsComponent,    
+    component: ContactComponent,
   }
 ];
 
